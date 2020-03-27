422905636d439d8b2eedd1f5f6a134aca5bdcfa8
Reyansh <reyansh2511@gmail.com>
2020-03-27 19:03:57 +0530

    a

--- sketch.js
+++ sketch.js
@@ -1,62 +1,70 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-
-var engine, world;
-var box1, pig1;
-var backgroundImg;
-
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
-}
-
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-
-    
-    ground = new Ground(600,height,1200,20)
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
+function checkForWinner() {
+  console.log("checking for winner...")
+  var winner
+  var playerOne = getPlayerOne() //playerOne is always blue!
+    //console.log('checkForWinner, playerOne is: ', playerOne)
+var computer = (playerOne === "X") ? "O" : "X"
+//there are 8 winningCombos: 
+  //three rows, three columns, two diagonals
+  //refactor below.... this is not DRY!
+  var blueWin1 = $("#one.blue, #two.blue, #three.blue").length === 3
+  var blueWin2 = $("#four.blue, #five.blue, #six.blue").length === 3
+  var blueWin3 = $("#seven.blue, #eight.blue, #nine.blue").length === 3
+  var blueWin4 = $("#one.blue, #four.blue, #seven.blue").length === 3
+  var blueWin5 = $("#two.blue, #five.blue, #eight.blue").length === 3
+  var blueWin6 = $("#three.blue, #six.blue, #nine.blue").length === 3
+  var blueWin7 = $("#one.blue, #five.blue, #nine.blue").length === 3
+  var blueWin8 = $("#seven.blue, #five.blue, #three.blue").length === 3
+var redWin1 = $("#one.red, #two.red, #three.red").length === 3
+  var redWin2 = $("#four.red, #five.red, #six.red").length === 3
+  var redWin3 = $("#seven.red, #eight.red, #nine.red").length === 3
+  var redWin4 = $("#one.red, #four.red, #seven.red").length === 3
+  var redWin5 = $("#two.red, #five.red, #eight.red").length === 3
+  var redWin6 = $("#three.red, #six.red, #nine.red").length === 3
+  var redWin7 = $("#one.red, #five.red, #nine.red").length === 3
+  var redWin8 = $("#seven.red, #five.red, #three.red").length === 3
+//var winningCombos = [x]
+  //refactor below too... not DRY at all!!!
+  //note: playerOne is always blue
+  var blueWins = (blueWin1 || blueWin2 || blueWin3 || blueWin4 || blueWin5 || blueWin6 || blueWin7 || blueWin8)
+var redWins = (redWin1 || redWin2 || redWin3 || redWin4 || redWin5 || redWin6 || redWin7 || redWin8)
+var redCount = getRedCount()
+    //console.log('redCount is: ', redCount)
+  var blueCount = getBlueCount()
+    //console.log('blueCount is: ', blueCount)
+  var fullGrid = redCount + blueCount
+  console.log('fullGrid is: ', fullGrid)
+var draw = (fullGrid === 9) && (!blueWins) && (!redWins)
+if (blueWins) { //playerOne is always blue
+    winner = blueWins
+    console.log(`${playerOne} wins!`)
+    $("#gameResult, #congratsOrSorry").removeClass("displayNone")
+    $("#gameResult").html(`<span class='yellowBig'>${playerOne} wins!</span>`)
+    $("#congratsOrSorry").html("<span class='yellow'>Congratulations! You won!</span>")
+    $("#gameInfo").addClass("displayNone")
+    disableRemainingItems()
+    return winner
+  }
+  if (redWins) { //red is computer
+    winner = redWins
+    console.log(`${computer} wins!`)
+    $("#gameResult, #congratsOrSorry").removeClass("displayNone")
+    $("#gameResult").html(`<span class='redBig'>${computer} wins!</span>`)
+    $("#congratsOrSorry").html("<span class='red'>Sorry, you lost.</span>")
+    $("#gameInfo").addClass("displayNone")
+    disableRemainingItems()
+    return winner
+  }
+  if (draw) {
+    winner = draw
+    console.log('Draw game!')
+    $("#gameResult, #congratsOrSorry").removeClass("displayNone")
+    $("#gameResult").html(`<span class='redBig'>Game is a draw.</span>`)
+    $("#congratsOrSorry").html("<span>Game ended in a draw.</span>")
+    $("#gameInfo").addClass("displayNone")
+    disableRemainingItems()
+    return winner
+  } else {
+    console.log('game on...')
+  }
 }
